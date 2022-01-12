d08b56befae0d8ad1810a6a10b35417e6f3a505e
jhoon9494 <wjdgns9406@gmail.com>
2022-01-12 15:33:09 +0900

    study useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const onClick = () => setCounter((prev) => prev + 1);
+  const [key, setKey] = useState("");
+  const onChange = (event) => setKey(event.target.value);
+  useEffect(() => {
+    console.log("CALL THE API");
+  }, []);
+  useEffect(() => {
+    if (key !== "") {
+      console.log("search for", key);
+    } else {
+      console.log("please search..");
+    }
+  }, [counter]);
   return (
     <div>
-      <h1 className={styles.title}>Welcome back React!</h1>
-      <Button text={"continue"} />
+      <input onChange={onChange} value={key} type="text" placeholder="Search..."></input>
+      <h1 className={styles.title}>{counter}</h1>
+      <Button text={"continue"} click={onClick} />
     </div>
   );
 }
--- src/Button.js
+++ src/Button.js
@@ -1,12 +1,16 @@
-import propTypes from "prop-types";
+import PropTypes from "prop-types";
 import styles from "./Button.module.css";
 
-function Button({ text }) {
-  return <button className={styles.btn}>{text}</button>;
+function Button({ text, click }) {
+  return (
+    <button onClick={click} className={styles.btn}>
+      {text}
+    </button>
+  );
 }
 
 Button.propTypes = {
-  text: propTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
 };
 
 export default Button;

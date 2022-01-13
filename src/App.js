7ddead7b6e58c619e1f501ce1b01f857fb6e467b
jhoon9494 <wjdgns9406@gmail.com>
2022-01-13 21:43:32 +0900

    Coin Tracker

--- src/App.js
+++ src/App.js
@@ -1,27 +1,59 @@
-import { useState } from "react/cjs/react.development";
+import { useEffect, useState } from "react/cjs/react.development";
 
 function App() {
-  const [todo, setTodo] = useState("");
-  const [todoArray, setTodoArray] = useState([]);
-  const onChange = (event) => setTodo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    setTodoArray((prevArray) => [todo, ...prevArray]);
-    setTodo("");
+  const [load, setLoad] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((data) => {
+        setCoins(data);
+        setLoad(false);
+      });
+  }, []);
+
+  const [money, setMoney] = useState(0);
+  const inputMoney = (event) => {
+    setMoney(event.target.value);
   };
+  const [selected, setSelected] = useState("select coin!");
+  const selectedOpt = (event) => {
+    setSelected(event.target.value);
+  };
+
+  const [hide, setHide] = useState(true); //계산 결과 숨기기/표시하기
+  const onClick = () => {
+    if (money > 0 && selected !== "select coin!") {
+      setHide(false);
+    }
+  };
+
   return (
     <div>
-      <h1>todo list</h1>
-      <form onSubmit={onSubmit}>
-        <input onChange={onChange} value={todo} type="text" placeholder="write your to do"></input>
-        <button>click</button>
-      </form>
-      <hr />
-      <ul>
-        {todoArray.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <h1>The Coins!({coins.length})</h1>
+      {load ? (
+        <strong>loading...</strong>
+      ) : (
+        <div>
+          <select onChange={selectedOpt} value={selected}>
+            <option>select coin!</option>
+            {coins.map((item) => (
+              <option key={item.id}>
+                {item.name} ({item.symbol}) : ${item.quotes.USD.price}
+              </option>
+            ))}
+          </select>
+          <hr />
+          <span>보유 금액($): </span>
+          <input type="number" placeholder="write your money" value={money} onChange={inputMoney}></input>
+          <button onClick={onClick}>계산</button>
+          {hide ? null : (
+            <h2>{`$${money}로 
+            ${selected.substring(selected.indexOf("("), selected.indexOf(")") + 1)}코인을 
+            ${money / selected.substring(selected.indexOf("$") + 1)}개 살 수 있습니다.`}</h2>
+          )}
+        </div>
+      )}
     </div>
   );
 }

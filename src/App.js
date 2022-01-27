fb4e421657f14b41a187c3ea8c8651a42bcb5fcb
jhoon9494 <wjdgns9406@gmail.com>
2022-01-27 10:43:48 +0900

    Published

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import Detail from "./routes/Detail";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="movie/:id" element={<Detail />} />
-        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
+        <Route path={"/"} element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
